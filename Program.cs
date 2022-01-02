60fe6ee657253c950fc51a3e75933e41502c2848
Axro-psy <76491158+Axro-psy@users.noreply.github.com>
2022-01-02 16:13:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWORLD_domashka_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, ");
+            Console.Write("WoRlD");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
